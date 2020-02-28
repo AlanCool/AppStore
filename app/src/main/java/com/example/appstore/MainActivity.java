5dd3285854e4c1dd2c9b4ee96d5054fcfcaab526
AlanCool <zhuxiaofeng023@163.com>
2020-02-28 20:45:01 +0800

    添加侧滑菜单

--- app/src/main/java/com/example/appstore/MainActivity.java
+++ app/src/main/java/com/example/appstore/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.appstore;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    View headerView;
+    @BindView(R.id.navigation_view)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        headerView=navigationView.getHeaderView(0);
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "HeaderView", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "header", Toast.LENGTH_SHORT).show();
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.application:
+                        Toast.makeText(MainActivity.this, "application", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.message:
+                        Toast.makeText(MainActivity.this, "message", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.setting:
+                        Toast.makeText(MainActivity.this, "setting", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+
+            }
+        });
     }
+
+
 }
